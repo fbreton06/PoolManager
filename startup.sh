aa10b2ddee5a6713e5b8f3e1c699e2683728874e
Breton Frederic <breton.frederic@gmail.com>
2017-12-23 15:17:37 +0100

    update startup script

--- startup.sh
+++ startup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-
-cd /home/pi/python/PoolSurvey;
-if [ -d /home/pi/python/PoolSurvey/PoolSurvey_new ]; then
-	cp /media/pi/data/database.ini /home/pi/python/PoolSurvey/PoolSurvey;
-	if [ -f /home/pi/python/PoolSurvey/PoolSurvey_new/update.py ]; then
-		mv /home/pi/python/PoolSurvey/PoolSurvey_new/update.py /home/pi/python/PoolSurvey/;
-		python /home/pi/python/PoolSurvey/PoolSurvey_new/update.py /media/pi/data database.ini;
+ref_path='/home/pi/python'
+data_path='/media/pi/data'
+cd $ref_path;
+# Check if an update is needed
+if [ -d $ref_path/PoolSurvey_new ]; then
+	# Save database file first for a next restoration
+	cp -f $data_path/database.ini $ref_path/PoolSurvey;
+	# If few treatments are necessary do them
+	if [ -f $ref_path/PoolSurvey_new/update.py ]; then
+		mv $ref_path/PoolSurvey_new/update.py $ref_path;
+		python $ref_path/update.py $data_path database.ini;
+		rm -f $ref_path/update.py
 	fi
-	echo "Switch to new server version.";
-	mv /home/pi/python/PoolSurvey/PoolSurvey /home/pi/python/PoolSurvey/PoolSurvey_bak;
-	mv /home/pi/python/PoolSurvey/PoolSurvey_new /home/pi/python/PoolSurvey/PoolSurvey;
+	# Switch to new server version but keep old in case of...
+	mv $ref_path/PoolSurvey $ref_path/PoolSurvey_bak;
+	mv $ref_path/PoolSurvey_new $ref_path/PoolSurvey;
 else
-	if [ -d /home/pi/python/PoolSurvey/PoolSurvey_bak ]; then
-		echo "Restore previous version because the new server version had not started!";
-		rm -frd /home/pi/python/PoolSurvey/PoolSurvey;
-		mv /home/pi/python/PoolSurvey/PoolSurvey_bak /home/pi/python/PoolSurvey/PoolSurvey;
-		mv /home/pi/python/PoolSurvey/PoolSurvey/database.ini /media/pi/data/;
+	# If after a reboot, an old version is detected restore it
+	if [ -d $ref_path/PoolSurvey_bak ]; then
+		# Restore previous version because the new server version had not started!
+		rm -frd $ref_path/PoolSurvey;
+		mv $ref_path/PoolSurvey_bak $ref_path/PoolSurvey;
+		mv $ref_path/PoolSurvey/database.ini $data_path;
 		sudo reboot;
 	fi
 fi
-cd /home/pi/python/PoolSurvey/PoolSurvey;
+cd $ref_path/PoolSurvey;
 echo "Start server...";
-python /home/pi/python/PoolSurvey/PoolSurvey/server.py;
-
- 
+python $ref_path/server.py;
 
